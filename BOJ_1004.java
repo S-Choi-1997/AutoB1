429db7e0e9729bb7e919cd58bdf956cae188caa0
최승호 <148317140+S-Choi-1997@users.noreply.github.com>
2025-01-10 16:14:02 +0900

    Add BOJ_1004.java

---
+++ BOJ_1004.java
@@ -0,0 +1,9 @@
+블로그 내용이 제공되지 않아서 일반적인 Java 클래스를 작성할 수 없습니다. 블로그 요약에 대한 다시 제공해주시면, 그것에 맞는 Java 코드를 제공할 수 있습니다. 그러나, 아래는 단순한 Java 코드 예제입니다:
+
+```java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+```
