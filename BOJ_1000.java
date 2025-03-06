f731fab9402d7595345c65ee649e95338ac0ee2b
최승호 <148317140+S-Choi-1997@users.noreply.github.com>
2025-03-07 04:58:47 +0900

    Update BOJ_1000.java

--- BOJ_1000.java
+++ BOJ_1000.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        System.out.print(A + B);
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
-		System.out.println(a + b);
-		br.close();
-	}
+        br.close();
+    }
 }
