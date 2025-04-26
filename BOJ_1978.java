f0e760ca4cf1d1bd31370e76ec1ae7f6d1b2afe9
최승호 <148317140+S-Choi-1997@users.noreply.github.com>
2025-04-26 23:00:26 +0900

    Add BOJ_1978.java

---
+++ BOJ_1978.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < num; i++) {
+            int isPN = Integer.parseInt(st.nextToken());
+            if (isPrime(isPN)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
